07bb7a2c992433b08fd1ed0ca60fa1f23695f221
Daimon-J <daimon.java.best@gmail.com>
2021-11-11 13:57:43 +0200

    1.2. Вызов метода объекта [#166085 #197068]

--- src/main/java/ru/job4j/tracker/oop/Student.java
+++ src/main/java/ru/job4j/tracker/oop/Student.java
@@ -2,21 +2,23 @@ package ru.job4j.tracker.oop;
 
 public class Student {
 
-    public void music() {
-        System.out.println("Tra tra tra");
+    public void music(String lyrics) {
+        System.out.println(lyrics);
     }
 
-    public void song() {
-        System.out.println("I believe I can fly");
+    public void song(String lyrics) {
+        System.out.println(lyrics);
     }
 
     public static void main(String[] args) {
         Student daimon = new Student();
-        daimon.music();
-        daimon.song();
-        daimon.music();
-        daimon.song();
-        daimon.music();
-        daimon.song();
+        String song = "I believe, I can fly";
+        String song2 = "And you can";
+        daimon.music(song);
+        daimon.song(song2);
+        daimon.music(song);
+        daimon.song(song2);
+        daimon.music(song);
+        daimon.song(song2);
     }
 }

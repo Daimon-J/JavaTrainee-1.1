721fdb31a5ed340797d9d6cda241c08e46cf2bec
Daimon-J <daimon.java.best@gmail.com>
2021-11-11 13:41:39 +0200

    1.1. Создание объекта [#165452 #197055]

---
+++ src/main/java/ru/job4j/tracker/oop/Dog.java
@@ -0,0 +1,9 @@
+package ru.job4j.tracker.oop;
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog rex = new Dog();
+        Dog polkan = new Dog();
+        Dog laika = new Dog();
+    }
+}

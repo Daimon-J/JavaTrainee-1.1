2d2845d12fa8fb62911f599726910a2db044b3d0
Daimon-J <daimon.java.best@gmail.com>
2021-11-12 11:54:52 +0200

    1.9. Статические и не статические методы [#504806]

---
+++ src/main/java/ru/job4j/tracker/oop/Calculator.java
@@ -0,0 +1,33 @@
+package ru.job4j.tracker.oop;
+
+public class Calculator {
+
+    private static int x = 5;
+
+    public static int sum(int y) {
+        return x + y;
+    }
+
+    public static int minus(int p) {
+        return x - p;
+    }
+
+    public int divide(int d) {
+        return x / d;
+    }
+
+    public int multiply(int a) {
+        return x * a;
+    }
+
+    public int sumAllOperation(int z) {
+        return sum(z) + minus(z) + divide(z) + multiply(z);
+    }
+
+    public static void main(String[] args) {
+        Calculator nonStaticSumAll = new Calculator();
+        nonStaticSumAll.sumAllOperation(47);
+        System.out.println("Result: " + nonStaticSumAll);
+
+    }
+}
